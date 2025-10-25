4e531bf5013db620cf7fc1b8e7805f7e62627973
victornext <victorbrs517@gmail.com>
2025-10-25 00:01:45 -0300

    Comecando as brincadeiras em JPA

---
+++ AprofundandoJPA/src/main/java/com/cursoJPA/AprofundandoJPA/AprofundandoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.cursoJPA.AprofundandoJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprofundandoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprofundandoJpaApplication.class, args);
+	}
+
+}
---
+++ AprofundandoJPA/src/test/java/com/cursoJPA/AprofundandoJPA/AprofundandoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursoJPA.AprofundandoJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AprofundandoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
